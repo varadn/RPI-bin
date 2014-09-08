59dc8c23f4a43a9dfbdeb83c29908b85e7ad1638
varadn <varadnev@gmail.com>
2014-09-07 18:16:16 -0700

    first commit

---
+++ bashlogout.sh
@@ -0,0 +1,12 @@
+# ~/.bash_logout: executed by bash(1) when login shell exits.
+
+if [ "$SHLVL" = 1 ]; then
+	echo "export HIGHSCORE=$HIGHSCORE" > /home/pi/.store_highscore.sh
+
+	echo "..." >> /home/pi/.log.txt
+	echo "Logoff" >>  /home/pi/.log.txt
+	date >>  /home/pi/.log.txt
+	echo "..." >>  /home/pi/.log.txt
+	echo " " >>  /home/pi/.log.txt
+	[ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
+fi
---
+++ store_highscore.sh
@@ -0,0 +1,1 @@
+export HIGHSCORE=3
